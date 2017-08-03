4de1535c5bf4748708df3f4e3f1fce869856180c
Alexashooo <aleksandar.sunjic@gmail.com>
2017-08-03 15:26:51 +0200

    Face lifting is finished

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
+  validates :recipient_email, presence: true
+  validates :subject,  length: { minimum: 5 }, presence: true
 
   scope :visible_to, -> (user) do
       return all.where('messages.user_id = ? OR messages.recipient_email = ?', user.id, user.email)
